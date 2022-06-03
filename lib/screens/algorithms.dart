b7424ada9af85a63dcc9d2ff7cb3dd1548b939b6
lele <lele204@gmx.de>
2022-06-04 00:09:19 +0200

    error fixes

--- lib/screens/algorithms.dart
+++ lib/screens/algorithms.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sort_demo/widgets/screen_header.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class Algorithms extends StatelessWidget {
   const Algorithms({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class Algorithms extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          const ScreenHeader('Algorithmen'),
+          ScreenHeader(AppLocalizations.of(context)!.algorithms),
           SliverList(
             delegate: SliverChildListDelegate(
               [],
